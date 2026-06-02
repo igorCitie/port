3945136890410d3f1e0faf52dab7dd0f6c95e130
igor lima <igor.pereira@citiesoft.com.br>
2026-06-02 15:38:42 -0300

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Contact from './components/sections/Contact/Contact'
 import ProjectDetail from './components/sections/ProjectDetail'
 import Footer from './components/ui/Footer/Footer'
 import About from './components/sections/about'
+import { Analytics } from "@vercel/analytics/next"
 function Home() {
   return (
     <>
